965e81662347c248660f850342891d8ba907deb5
devankitx <126304960+devankitx@users.noreply.github.com>
2025-03-02 22:46:08 +0530

    added qr codes to all

--- src/data/qrCodes.js
+++ src/data/qrCodes.js
@@ -1,10 +1,17 @@
 const path = require("path");
 const { IMAGES_FOLDER } = require("../config/constants");
+const pdfs = require("./pdfs");
 
-const QR_CODES = {
-  cengage_maths_chapter1: path.join(IMAGES_FOLDER, "19_payment_qr.jpg"),
-  cengage_maths_chapter2: path.join(IMAGES_FOLDER, "19_payment_qr.jpg"),
-  default: path.join(IMAGES_FOLDER, "coming_soon.jpg"),
-};
+const QR_CODES = {};
+
+// Helper function to set QR codes for available PDFs
+Object.keys(pdfs).forEach((key) => {
+  if (pdfs[key] !== "PDF coming soon") {
+    QR_CODES[key] = path.join(IMAGES_FOLDER, "19_payment_qr.jpg");
+  }
+});
+
+// Set default for unavailable PDFs
+QR_CODES.default = path.join(IMAGES_FOLDER, "coming_soon.jpg");
 
 module.exports = QR_CODES;
